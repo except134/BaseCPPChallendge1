3119281f7a036b963b22a861bcc5b398b2b676a8
except134 <except134@gmail.com>
2021-05-28 08:20:25 +0300

    test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
+	HWND hwnd = GetConsoleWindow();
+	HDC hdc = GetDC(hwnd);
+
+	int x = 0;
+	for (float i = 0; i < 3.14 * 10; i += 0.05)
+	{
+		SetPixel(hdc, x, 50 + 25 * cos(i), RGB(255, 255, 255));
+		x += 1;
+	}
+
+	ReleaseDC(hwnd, hdc);
+	std::cin.ignore();
+	return 0;
+
 	Game game;
 	return game.Run();
 }
